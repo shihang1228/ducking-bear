2e6a8303685f7dd00498b5b130e6e41abf27775c
shihang <study1228.sh@gmail.com>
2014-03-29 15:15:08 +0800

    引入了jdbc驱动

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 mkdir -p $CATALINA_HOME/webapps/bear/
 cp -r ./src/main/webapps/* $CATALINA_HOME/webapps/bear/
-cp -r ./target/classes $CATALINA_HOME/webapps/bear/WEB-INF/
-
-
+cp -r ./target/classes $CATALINA_HOME/webapps/bear/WEB-INF
+cp -r ./lib $CATALINA_HOME/webapps/bear/WEB-INF/
