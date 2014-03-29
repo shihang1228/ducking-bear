ab7ffaf4696cf8e86fc17ec91276e23d6a212e64
shihang <study1228.sh@gmail.com>
2014-03-29 15:31:08 +0800

    实现了链接数据库的功能

--- src/main/java/com/bodejidi/Management.java
+++ src/main/java/com/bodejidi/Management.java
@@ -5,6 +5,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import javax.servlet.ServletException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 
 public class Management extends HttpServlet
 {
@@ -18,6 +22,32 @@ public class Management extends HttpServlet
         resp.setContentType("text/html; charset = UTF-8");
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-        resp.getWriter().println( username +"   "+ password);
+        //resp.getWriter().println( username +"   "+ password + " success!!!");
+        
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        }catch(Exception ex)
+        {
+          //ignore;
+        }
+            Connection conn = null;
+            Statement stmt = null;
+        
+        try
+        {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?"
+                                                 + "user=root"
+                                                 + "&password=");
+            stmt = conn.createStatement();
+            resp.getWriter().println("haha!hehe!");
+        
+        }catch(SQLException ex)
+        {
+            System.out.println("SQLExcepton: " + ex.getMessage());
+            System.out.println("SQLStates: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            resp.getWriter().println("error!");
+        }
     }
 }
