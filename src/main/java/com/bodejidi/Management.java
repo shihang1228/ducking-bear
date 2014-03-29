79d55bd74cba058bb5d12177d276ca5b14f53d02
shihang <study1228.sh@gmail.com>
2014-03-29 13:18:26 +0800

    再次修改格式

--- src/main/java/com/bodejidi/Management.java
+++ src/main/java/com/bodejidi/Management.java
@@ -8,11 +8,11 @@ import javax.servlet.ServletException;
 
 public class Management extends HttpServlet
 {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
     {
 		//resp.getWriter().println("shihanghangxing");
     }
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
     {
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset = UTF-8");
