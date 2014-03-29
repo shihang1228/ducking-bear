a0d892e98b2f7b1460921141d09fc764b62ae7e4
shihang <study1228.sh@gmail.com>
2014-03-29 12:58:04 +0800

    以UTF-8编码打印出用户名和密码

--- src/main/java/com/bodejidi/Management.java
+++ src/main/java/com/bodejidi/Management.java
@@ -10,6 +10,14 @@ public class Management extends HttpServlet
 {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
 	{
-		resp.getWriter().println("shihanghangxing");
+		//resp.getWriter().println("shihanghangxing");
+	}
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
+	{
+        req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset = UTF-8");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		resp.getWriter().println( username +"   "+ password);
 	}
 }
