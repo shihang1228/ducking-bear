ca82a98dd6cb904e887516b6b58a61acb0931af8
shihang <study1228.sh@gmail.com>
2014-03-29 13:14:34 +0800

    修改格式

--- src/main/java/com/bodejidi/Management.java
+++ src/main/java/com/bodejidi/Management.java
@@ -9,15 +9,15 @@ import javax.servlet.ServletException;
 public class Management extends HttpServlet
 {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
-	{
+    {
 		//resp.getWriter().println("shihanghangxing");
-	}
+    }
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException
-	{
+    {
         req.setCharacterEncoding("UTF-8");
-		resp.setContentType("text/html; charset = UTF-8");
-		String username = req.getParameter("username");
-		String password = req.getParameter("password");
-		resp.getWriter().println( username +"   "+ password);
-	}
+        resp.setContentType("text/html; charset = UTF-8");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        resp.getWriter().println( username +"   "+ password);
+    }
 }
